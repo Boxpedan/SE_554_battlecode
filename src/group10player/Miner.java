aeeb89a101f7d694b6f10a30fff0d1d54c09db9c
Boxpedan <boxuan@pdx.edu>
2020-10-14 13:03:22 -0700

    add new constructor

--- src/group10player/Miner.java
+++ src/group10player/Miner.java
@@ -2,11 +2,12 @@ package group10player;
 import battlecode.common.*;
 
 public class Miner extends Unit{
-    public Miner(RobotController r) {
+    public Miner(RobotController r) throws GameActionException {
         super(r);
     }
 
     public void takeTurn() throw GameActionException{
+        super();
 
     }
 
