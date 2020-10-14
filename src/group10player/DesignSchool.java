52401502953034a27880013b0c816c4fd097a52e
alex-bailey1 <bail34@pdx.edu>
2020-10-14 12:57:40 -0700

    adding basic concstructor

--- src/group10player/DesignSchool.java
+++ src/group10player/DesignSchool.java
@@ -1,5 +1,10 @@
 package group10player;
 import battlecode.common.*;
 
-public class DesignSchool {
+public class DesignSchool extends Building {
+
+    public DesignSchool(RobotController rc) throws GameActionException
+    {
+        super(rc);
+    }
 }
