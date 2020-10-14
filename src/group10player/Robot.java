4e8429f8123a8da699c3907875fad467a51b0fe7
cmaxwell2 <56896619+cmaxwell2@users.noreply.github.com>
2020-10-14 13:02:45 -0700

    Added exception throwing to Robot, Unit, and Building

--- src/group10player/Robot.java
+++ src/group10player/Robot.java
@@ -4,12 +4,24 @@ import battlecode.common.*;
 
 //Robot will receive "rc" as a variable in its constructor.
 public class Robot {
+
+    static Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST
+    };
+
     RobotController rc;
     public Robot(RobotController rcTemp) throws GameActionException{
         rc = rcTemp;
     }
 
-    public void takeTurn(){
+    public void takeTurn() throws GameActionException{
 
     }
 }
