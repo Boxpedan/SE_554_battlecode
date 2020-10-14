7998f52d1f50708dc2deee54107e96e366da3054
cmaxwell2 <56896619+cmaxwell2@users.noreply.github.com>
2020-10-14 12:46:04 -0700

    Refinery now extends Building

--- src/group10player/Refinery.java
+++ src/group10player/Refinery.java
@@ -1,6 +1,9 @@
 package group10player;
 import battlecode.common.*;
 
-public class Refinery {
+public class Refinery extends Building{
     //this is to test Github
+    public Refinery(RobotController rc) throws GameActionException{
+        super(rc);
+    }
 }
