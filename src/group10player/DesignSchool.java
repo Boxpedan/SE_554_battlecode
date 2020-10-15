ace9ba20ae5c38bfb47875acfa319d86d3d494b4
alex-bailey1 <bail34@pdx.edu>
2020-10-15 15:41:12 -0700

    design school produces 1 landscapper

--- src/group10player/DesignSchool.java
+++ src/group10player/DesignSchool.java
@@ -2,7 +2,8 @@ package group10player;
 import battlecode.common.*;
 
 public class DesignSchool extends Building {
-
+    int numLandscappers = 0;
+    int landscapperLimit = 1;
     public DesignSchool(RobotController rc) throws GameActionException
     {
         super(rc);
@@ -10,6 +11,20 @@ public class DesignSchool extends Building {
 
     @Override
     public void takeTurn() throws GameActionException{
+        if(numLandscappers < landscapperLimit)
+        {
+            if(rc.getTeamSoup() >= 150)
+            {
+                for(Direction dir : directions)
+                {
+                    if(tryBuild(RobotType.LANDSCAPER, dir))
+                    {
+                        numLandscappers++;
+                        break;
+                    }
+                }
+            }
+        }
 
     }
 }
