f6156800c612edb6a538c56bb1cf184f207b5ca2
cmaxwell2 <56896619+cmaxwell2@users.noreply.github.com>
2020-10-26 11:36:05 -0700

    Added in blockchain functionality for sending HQ location, finding HQ location, and sending refinery locations.

--- src/group10player/HQ.java
+++ src/group10player/HQ.java
@@ -5,13 +5,29 @@ public class HQ extends Building{
     int numMiners = 0;
     int limitMiners = 4;
     int MAX_LIMIT_MINER = 10;
+    boolean hasSentLocation;
 
     public HQ(RobotController rc) throws GameActionException {
         super(rc);
+        hasSentLocation = false;
     }
     
     @Override
     public void takeTurn() throws GameActionException{
+        if (!hasSentLocation){
+            int [] HQLocationMessage = new int[7]; // formatted {159, 000, XPOS, YPOS, 000, 000, 000}
+            for (int x = 0; x < 7; x++){
+                HQLocationMessage[x] = 0;
+            }
+            HQLocationMessage[0] = teamMessageCode;
+            HQLocationMessage[2] = rc.getLocation().x;
+            HQLocationMessage[3] = rc.getLocation().y;
+
+            if (rc.canSubmitTransaction(HQLocationMessage, 29)){ //we're willing to pay 29, in order to make sure
+                rc.submitTransaction(HQLocationMessage, 29); //that no strategies for flooding the blockchain on turn 1
+                hasSentLocation = true;                         //make it so our message isn't sent.
+            }
+        }
 
         if(numMiners < limitMiners){
             for (Direction dir: Robot.directions){
