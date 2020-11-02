3148c199e2ef8022f87c7cb7254de01e8006bca1
alex-bailey1 <bail34@pdx.edu>
2020-11-01 16:21:47 -0800

    changing test

--- src/group10player/DeliveryDrone.java
+++ src/group10player/DeliveryDrone.java
@@ -49,7 +49,7 @@ public class DeliveryDrone extends Unit {
         System.out.println("myTeam.opponent(): " + myTeam.opponent());
         System.out.println("Team.B: " + Team.B);
         System.out.println(myTeam.opponent() == getTeamOpponent());
-        RobotInfo[] enemy_robots = rc.senseNearbyRobots(24, myTeam.opponent());
+//        RobotInfo[] enemy_robots = rc.senseNearbyRobots(24, myTeam.opponent());
 
 //        System.out.println("enemy_robots: " + enemy_robots);
 
