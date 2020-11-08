f8ab48bf74d0d515428c893db8743d469a452495
Boxpedan <boxuan@pdx.edu>
2020-11-08 00:21:58 -0800

    change branch and add two classes

---
+++ src/group10player/Vaporator.java
@@ -0,0 +1,11 @@
+package group10player;
+import battlecode.common.*;
+
+public class Vaporator extends Building {
+    public Vaporator(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    public void takeTurn() throws GameActionException {
+    }
+}
