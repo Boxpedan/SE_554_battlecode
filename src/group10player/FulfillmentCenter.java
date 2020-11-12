c7ba5baac8e8f8dd4083b3692701cd04f56d2f13
cmaxwell2 <56896619+cmaxwell2@users.noreply.github.com>
2020-11-12 13:06:18 -0800

    Delivery drones are built once everything else is done

--- src/group10player/FulfillmentCenter.java
+++ src/group10player/FulfillmentCenter.java
@@ -4,9 +4,11 @@ import battlecode.common.*;
 public class FulfillmentCenter extends Building{
     int numDrones = 0;
     int droneLimit = 3;
+    boolean freeBuild;
     public FulfillmentCenter(RobotController rc) throws GameActionException
     {
         super(rc);
+        freeBuild = false;
     }
 
     @Override
@@ -14,6 +16,22 @@ public class FulfillmentCenter extends Building{
         if(numDrones < droneLimit)
         {
             if(rc.getTeamSoup() >= 155)
+            {
+                for(Direction dir : directions)
+                {
+                    if(tryBuild(RobotType.DELIVERY_DRONE, dir))
+                    {
+                        numDrones++;
+                        break;
+                    }
+                }
+            }
+        }
+        if (rc.getTeamSoup() > 1000){
+            freeBuild = true;
+        }
+        if (freeBuild){
+            if(rc.getTeamSoup() >= 200)
             {
                 for(Direction dir : directions)
                 {
